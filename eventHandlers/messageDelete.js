98a0247837f7eaa20ccca35fae837b60a26418ad
JulioLP <julio@dynamox.net>
2018-03-26 19:56:54 -0300

    fix the messageDeleted message

--- eventHandlers/messageDelete.js
+++ eventHandlers/messageDelete.js
@@ -1,7 +1,7 @@
 module.exports = (message) => {
   const channel = message.guild.systemChannel;
 
-  channel.send(`Tentaram apagar uma mensagem é? por acaso o ${message.author} disse que: ${message}`,{
+  channel.send(`Tentaram apagar uma mensagem é? o ${message.author} disse: ${message}`,{
     tts: true
   })
 }
